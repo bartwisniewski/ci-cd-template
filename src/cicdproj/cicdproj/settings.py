8c86d13d2a5b0ef0fcf6e0a78d249f5cbb4e2a76
Bartosz Wisniewski <bartekwisnia@op.pl>
2023-11-07 13:34:31 +0100

    dockerfile and docker-compose

--- src/cicdproj/cicdproj/settings.py
+++ src/cicdproj/cicdproj/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = "django-insecure-8e*eh(76yhshi9!rs^h*@&nvj2g3uw_ntkb6zut-81_r5y4v5t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:8001"]
 
 # Application definition
 
@@ -74,12 +74,24 @@ WSGI_APPLICATION = "cicdproj.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if "DB_ENGINE" in os.environ:
+    DATABASES = {
+        "default": {
+            "ENGINE": os.environ["DB_ENGINE"],
+            "NAME": os.environ["DB_DB"],
+            "USER": os.environ["DB_USER"],
+            "PASSWORD": os.environ["DB_PASSWORD"],
+            "HOST": os.environ["DB_HOST"],
+            "PORT": os.environ["DB_PORT"],
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
